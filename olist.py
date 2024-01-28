a8c8b2aba1ccb5436218de72f78dc0f10f7cca51
alexislbh <alexis.lbh@gmail.com>
2024-01-28 09:39:07 +0100

    update app

--- olist.py
+++ olist.py
@@ -31,7 +31,6 @@ def join_tables(t1, t2, item):
     duckdb.sql(f'ALTER TABLE {table} DROP COLUMN "{item}_id:1"')
     return duckdb.sql(f"select * from {table}")
 
-<<<<<<< HEAD
 def dataframe_with_selections(df):
     df_with_selections = df.copy()
     df_with_selections.insert(0, "Select", False)
@@ -210,51 +209,3 @@ map.fit_bounds(map.get_bounds())
 
 # call to render Folium map in Streamlit
 st_data = folium_static(map, width=725)
-=======
-
-start = time.time()
-
-# import orders_customers_dataset
-order_customers_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/orders_customers_dataset.csv").drop(columns = "Unnamed: 0")\
-.rename(columns={"customer_zip_code_prefix": "zip_code_prefix_id"})
-duckdb.sql("CREATE OR REPLACE TABLE order_customers AS SELECT * FROM order_customers_df")
-
-# import geolocatio_dataset
-response = requests.get("https://github.com/WildCodeSchool/wilddata/raw/main/geolocation_dataset.zip")
-with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-    z.extractall()
-geolocation_df = pd.read_csv("geolocation_dataset.csv").drop(columns = "Unnamed: 0")\
-.rename(columns={"geolocation_zip_code_prefix": "zip_code_prefix_id"})
-geolocation_df['geolocation_city'] = geolocation_df['geolocation_city'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-duckdb.sql("CREATE OR REPLACE TABLE geolocation AS SELECT * FROM geolocation_df")
-
-# import order_items_dataset
-order_items_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/order_items_dataset.csv").drop(columns = "Unnamed: 0")
-duckdb.sql("CREATE OR REPLACE TABLE order_items AS SELECT * FROM order_items_df")
-
-# import order_payments_dataset
-order_payments_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/order_payments_dataset.csv").drop(columns = "Unnamed: 0")
-duckdb.sql("CREATE OR REPLACE TABLE order_payments AS SELECT * FROM order_payments_df")
-
-# import order_reviews_dataset
-order_reviews_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/order_reviews_dataset.csv").drop(columns = "Unnamed: 0")
-duckdb.sql("CREATE OR REPLACE TABLE order_reviews AS SELECT * FROM order_reviews_df")
-
-# import orders_dataset
-orders_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/orders_dataset.csv").drop(columns = "Unnamed: 0")
-duckdb.sql("CREATE OR REPLACE TABLE orders AS SELECT * FROM orders_df")
-
-# import products_dataset
-products_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/products_dataset.csv").drop(columns = "Unnamed: 0")
-duckdb.sql("CREATE OR REPLACE TABLE products AS SELECT * FROM products_df")
-
-# import sellers_dataset
-sellers_df = pd.read_csv("https://raw.githubusercontent.com/WildCodeSchool/wilddata/main/sellers_dataset.csv").drop(columns = "Unnamed: 0")\
-.rename(columns={"seller_zip_code_prefix": "zip_code_prefix_id"})
-duckdb.sql("CREATE OR REPLACE TABLE sellers AS SELECT * FROM sellers_df")
-
-print(time.time() - start)
-
-st.dataframe(sql('show tables').df().name)
-st.dataframe(sql('select * from geolocation').df().head(10))
->>>>>>> 2b9978e (update)
